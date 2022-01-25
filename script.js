21729dc0766d82e3c8c586a77af135e733b81ff7
emangomaa <emangomaa149@gmail.com>
2022-01-25 13:19:43 +0200

    add project files

---
+++ script.js
@@ -0,0 +1,9 @@
+var links = document.getElementById('links');
+        var menuBtn = document.getElementById('menu-btn');
+        var closeBtn = document.getElementById('close-btn');
+        menuBtn.addEventListener('click',function(){
+            links.style.right="0";
+        });
+        closeBtn.addEventListener('click',function(){
+            links.style.right="-50vw"
+        })
